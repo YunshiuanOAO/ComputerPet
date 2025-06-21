fb2f02c5d9c2d5765327ee177851e685a8df0b35
l-zch <linzich3n@gmail.com>
2025-06-21 21:58:52 +0800

    fix: remove debug print statement from patchPicturePath method

--- src/main/java/utils/PathTool.java
+++ src/main/java/utils/PathTool.java
@@ -3,7 +3,6 @@ package utils;
 public class PathTool {
     private static final String basePath = "/";
     public static String patchPicturePath(String imagePath) {
-        System.out.println(basePath + imagePath);
         return basePath + imagePath;
     }
 }
